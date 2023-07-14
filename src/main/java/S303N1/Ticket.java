01c6cfa57d8416614e0ad535c5a9db3dd5a1e325
Sprint1_IT <kathya19@hotmail.com>
2023-07-14 12:09:59 +0200

    subida ticket natalia

--- src/main/java/S303N1/Ticket.java
+++ src/main/java/S303N1/Ticket.java
@@ -1,5 +1,43 @@
 package S303N1;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 public class Ticket {
+
+    private static int contadorTickets = 0;
+    private int numeroTicket;
+    private Date fechaCompra;
+    private List<LineaTicket> lineas;
+    private double totalCompra;
+
+    //Constructor
+    public Ticket(Date fechaCompra) {
+        this.numeroTicket = contadorTickets++;
+        this.fechaCompra = fechaCompra;
+        this.lineas = new ArrayList<>();
+        this.totalCompra = 0.0;
+    }
+    //Getter
+    public int getNumeroTicket() {
+
+        return numeroTicket;
+    }
+    public Date getFechaCompra() {
+
+        return fechaCompra;
+    }
+    public List<LineaTicket> getLineas() {
+
+        return lineas;
+    }
+    public double getTotalCompra() {
+
+        return totalCompra;
+    }
+    public void agragarlinea(LineaTicket linea) {
+        lineas.add(linea);
+        totalCompra += linea.getImporte();
+    }
 
 }
