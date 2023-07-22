fe878ce96f6dd92f3904f5b215517260536f1ad1
Natalia Fara Ch <132767336+nfaraDev@users.noreply.github.com>
2023-07-22 12:48:38 +0200

    Update LineaTicket.java
    Cambios LineaTicket

--- src/main/java/S303N1/LineaTicket.java
+++ src/main/java/S303N1/LineaTicket.java
@@ -1,17 +1,21 @@
 package S303N1;
 
 public class LineaTicket {
+    /*implementación, la clase LineaTicket representa una línea de un ticket de compra y puede contener información como
+    el producto, la cantidad y el importe de esa línea*/
+    //Atributos
+    private Producto producto;//producto: representa el producto de la línea de ticket.
+    private int cantidad;//cantidad: representa la cantidad de ese producto en la línea de ticket.
+    private double importe;//importe: representa el importe total de esa línea de ticket, calculado multiplicando el precio del producto por la cantidad.
 
-    private Producto producto;
-    private int cantidad;
-    private double precio;
-
-    public LineaTicket(Producto producto, int cantidad, double precio) {
+    //Metodo Constructor
+    //recibe el producto y la cantidad, y calcula automáticamente el importe.
+    public LineaTicket(Producto producto, int cantidad) {
         this.producto = producto;
         this.cantidad = cantidad;
-        this.precio = precio;
+        this.importe = producto.getPrecio() * cantidad;
     }
-    //Getter
+
     public Producto getProducto() {
         return producto;
     }
@@ -20,12 +24,31 @@ public class LineaTicket {
         return cantidad;
     }
 
-    public double getPrecio() {
-        return precio;
-    }
-
     public double getImporte() {
-        double importe = 0;
         return importe;
     }
-}
+    //Setter
+    // setPrecio permite actualizar el precio del producto y recalcular el importe de la línea de ticket.
+    public void setPrecio(int nuevoPrecio) {
+        producto.setPrecio(nuevoPrecio);
+        calcularImporte();
+    }
+
+    private void calcularImporte() {
+        importe = producto.getPrecio() * cantidad;
+    }
+    //devuelve una representación en forma de cadena de la línea de ticket, mostrando el nombre del producto, la cantidad y el importe.
+    @Override
+    public String toString() {
+        return "Producto: " + producto.getNombre() +
+                ", Cantidad: " + cantidad +
+                ", Importe: " + importe;
+    }
+
+    public double getSubtotal() {
+        return getSubtotal();
+    }
+
+    public int calcularSubtotal() {
+        return 0;
+    }
